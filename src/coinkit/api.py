128f857018b99f131abfe14ec43213e3bf65c7e0
duyyudus <duyyudus@gmail.com>
2021-09-05 16:18:03 +0700

    Basic add, update, delete coin operations

--- src/coinkit/api.py
+++ src/coinkit/api.py
@@ -6,15 +6,14 @@ from flask_cors import cross_origin
 
 from .db import (
     DB,
-    PORTFOLIO_AMOUNT_KEY,
-    PORTFOLIO_CGID_KEY,
-    PORTFOLIO_KEY,
-    PORTFOLIO_NAME_KEY,
-    PORTFOLIO_PRICE_KEY,
+    COIN_USER_KEY,
+    COIN_AMOUNT_KEY,
+    COIN_CGID_KEY,
+    COIN_SYMBOL_KEY,
+    COIN_PRICE_KEY,
+    COIN_WALLET_TYPE_KEY,
     PWD_KEY,
     USERNAME_KEY,
-    USERS_KEY,
-    WALLET_TYPE_KEY,
 )
 
 
@@ -40,3 +39,107 @@ class ApiHandler(object):
                 'data': {},
                 'message': self._server.config['ABOUT'],
             }
+
+        @self._server.route('/add_coin', methods=['POST'])
+        @cross_origin()
+        def add_coin():
+            user = request.args.get(COIN_USER_KEY, type=str)
+            symbol = request.args.get(COIN_SYMBOL_KEY, type=str)
+            cgid = request.args.get(COIN_CGID_KEY, type=str)
+            amount = request.args.get(COIN_AMOUNT_KEY, type=float)
+            price = request.args.get(COIN_PRICE_KEY, type=float)
+            wallet_type = request.args.get(COIN_WALLET_TYPE_KEY, type=str)
+
+            self._server.logger.debug('Add coin:')
+            self._server.logger.debug(f'--User: {user}')
+            self._server.logger.debug(f'--Symbol: {symbol}')
+
+            if user and symbol:
+                coin_data = {
+                    COIN_USER_KEY: user,
+                    COIN_SYMBOL_KEY: symbol,
+                    COIN_CGID_KEY: cgid,
+                    COIN_AMOUNT_KEY: amount,
+                    COIN_PRICE_KEY: price,
+                    COIN_WALLET_TYPE_KEY: wallet_type,
+                }
+
+                status, data, message = self._db.add_coin(coin_data)
+
+            else:
+                status = Status.ERROR.value
+                data = {}
+                message = 'Please provide username and coin symbol'
+
+            return {'status': status, 'data': data, 'message': message}
+
+        @self._server.route('/update_coin', methods=['POST'])
+        @cross_origin()
+        def update_coin():
+            user = request.args.get(COIN_USER_KEY, type=str)
+            symbol = request.args.get(COIN_SYMBOL_KEY, type=str)
+            cgid = request.args.get(COIN_CGID_KEY, type=str)
+            amount = request.args.get(COIN_AMOUNT_KEY, type=float)
+            price = request.args.get(COIN_PRICE_KEY, type=float)
+            wallet_type = request.args.get(COIN_WALLET_TYPE_KEY, type=str)
+
+            self._server.logger.debug('Update coin:')
+            self._server.logger.debug(f'--User: {user}')
+            self._server.logger.debug(f'--Symbol: {symbol}')
+
+            if user and symbol:
+                coin_data = {
+                    COIN_USER_KEY: user,
+                    COIN_SYMBOL_KEY: symbol,
+                    COIN_CGID_KEY: cgid,
+                    COIN_AMOUNT_KEY: amount,
+                    COIN_PRICE_KEY: price,
+                    COIN_WALLET_TYPE_KEY: wallet_type,
+                }
+
+                status, data, message = self._db.update_coin(coin_data)
+
+            else:
+                status = Status.ERROR.value
+                data = {}
+                message = 'Please provide username and coin symbol'
+
+            return {'status': status, 'data': data, 'message': message}
+
+        @self._server.route('/delete_coin', methods=['POST'])
+        @cross_origin()
+        def delete_coin():
+            user = request.args.get(COIN_USER_KEY, type=str)
+            symbol = request.args.get(COIN_SYMBOL_KEY, type=str)
+
+            self._server.logger.debug('Delete coin:')
+            self._server.logger.debug(f'--User: {user}')
+            self._server.logger.debug(f'--Symbol: {symbol}')
+
+            if user and symbol:
+                status, data, message = self._db.delete_coin(user, symbol)
+
+            else:
+                status = Status.ERROR.value
+                data = {}
+                message = 'Please provide username and coin symbol'
+
+            return {'status': status, 'data': data, 'message': message}
+
+        @self._server.route('/ls_coins', methods=['GET'])
+        @cross_origin()
+        def ls_coins():
+            user = request.args.get(COIN_USER_KEY, type=str)
+
+            self._server.logger.debug('List coin:')
+            self._server.logger.debug(f'--User: {user}')
+
+            if user:
+                status, data, message = self._db.ls_coins(user)
+
+            else:
+                status = Status.ERROR.value
+                data = {}
+                message = 'Please provide username'
+
+            return {'status': status, 'data': data, 'message': message}
--- src/coinkit/db.py
+++ src/coinkit/db.py
@@ -1,20 +1,25 @@
+from enum import Enum
 from typing import List
 
 from bson.objectid import ObjectId
 from flask import Flask
-from pymongo import MongoClient
-from pymongo.results import DeleteResult, UpdateResult
+from pymongo import MongoClient, message
+from pymongo.results import DeleteResult, InsertOneResult, UpdateResult
 
-USERS_KEY = 'users'
 USERNAME_KEY = 'user'
 PWD_KEY = 'pwd'
 
-PORTFOLIO_KEY = 'portfolio'
-PORTFOLIO_CGID_KEY = 'cgid'
-PORTFOLIO_NAME_KEY = 'name'
-PORTFOLIO_AMOUNT_KEY = 'amount'
-PORTFOLIO_PRICE_KEY = 'price'
-WALLET_TYPE_KEY = 'wallet_type'
+COIN_USER_KEY = 'user'
+COIN_CGID_KEY = 'cgid'
+COIN_SYMBOL_KEY = 'symbol'
+COIN_AMOUNT_KEY = 'amount'
+COIN_PRICE_KEY = 'price'
+COIN_WALLET_TYPE_KEY = 'wallet_type'
+
+
+class Status(Enum):
+    SUCCESS = 'success'
+    ERROR = 'error'
 
 
 class DB(object):
@@ -25,7 +30,59 @@ class DB(object):
         self._db = MongoClient(mongo_uri).coinkit
         self._server = server
 
-    def add_coin(self, coin_data: dict) -> ObjectId:
-        res = self._db.coins.insert_one(coin_data)
+    def add_coin(self, coin_data: dict) -> tuple:
+        status = Status.SUCCESS.value
+        data = None
+        message = 'Added coin successfully'
+        try:
+            self._db.portfolio.insert_one(coin_data)
+        except Exception as e:
+            status = Status.ERROR.value
+            message = f'Add coin error: {e}'
+        return status, data, message
 
-        return res
+    def update_coin(self, coin_data: dict) -> tuple:
+        status = Status.SUCCESS.value
+        data = None
+        message = 'Updated coin successfully'
+        try:
+            self._db.portfolio.update_one(
+                {
+                    COIN_USER_KEY: coin_data[COIN_USER_KEY],
+                    COIN_SYMBOL_KEY: coin_data[COIN_SYMBOL_KEY],
+                },
+                {'$set': coin_data},
+            )
+        except Exception as e:
+            status = Status.ERROR.value
+            message = f'Update coin error: {e}'
+        return status, data, message
+
+    def delete_coin(self, user: str, symbol: str) -> tuple:
+        status = Status.SUCCESS.value
+        data = None
+        message = 'Deleted coin successfully'
+        try:
+            self._db.portfolio.delete_one(
+                {
+                    COIN_USER_KEY: user,
+                    COIN_SYMBOL_KEY: symbol,
+                }
+            )
+        except Exception as e:
+            status = Status.ERROR.value
+            message = f'Delete coin error: {e}'
+        return status, data, message
+
+    def ls_coins(self, user: str) -> tuple:
+        status = Status.SUCCESS.value
+        data = []
+        message = 'Listed coin successfully'
+        try:
+            for t in self._db.portfolio.find({COIN_USER_KEY: user}):
+                t.pop('_id')
+                data.append(t)
+        except Exception as e:
+            status = Status.ERROR.value
+            message = f'List coin error: {e}'
+        return status, data, message if data else 'User not found'
